0a7131ff70852a4787d8db264e3783b137d351dd
Ken Sodemann <ken@ionic.io>
2022-08-08 07:47:00 -0500

    chore(): make the code prettier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'auth-playground-vue',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
