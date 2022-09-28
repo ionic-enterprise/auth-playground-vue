6c2bb1100fcfba30e5cb6610e918746ef0efde97
Ken Sodemann <ken@ionic.io>
2022-09-28 15:22:14 -0500

    feat(about): add information to the about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -10,19 +10,59 @@
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ExploreContainer name="About page" />
+    <ion-content>
+      <ion-list>
+        <ion-list-header>About</ion-list-header>
+        <ion-item>
+          <ion-label>Name</ion-label>
+          <ion-note slot="end">{{ name }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Author</ion-label>
+          <ion-note slot="end">{{ author }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Version</ion-label>
+          <ion-note slot="end">{{ version }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Auth Connect</ion-label>
+          <ion-note slot="end">{{ authConnectVersion }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Identity Vault</ion-label>
+          <ion-note slot="end">{{ identityVaultVersion }}</ion-note>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonNote,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
 import { logOutOutline } from 'ionicons/icons';
 import useAuth from '@/composables/auth';
 import useSessionVault from '@/composables/session-vault';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import packageInfo from '../../package.json';
+
+const { author, name, version } = packageInfo;
+const authConnectVersion = packageInfo.dependencies['@ionic-enterprise/auth'];
+const identityVaultVersion = packageInfo.dependencies['@ionic-enterprise/identity-vault'];
 
 const router = useRouter();
 const { logout } = useAuth();
