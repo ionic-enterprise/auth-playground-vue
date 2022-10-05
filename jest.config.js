2d73af25f4462528893c89abecb1b61b79bec6c0
Ken Sodemann <ken@ionic.io>
2022-10-05 12:09:54 -0500

    chore(): upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transformIgnorePatterns: [
-    '/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|@ionic-enterprise|ionicons)',
+    '/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|@ionic-enterprise|ionicons|axios)',
   ],
 };
