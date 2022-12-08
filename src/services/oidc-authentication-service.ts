8bd025b3fea57bab0168d6432d68e4e22e0e3703
Ken Sodemann <ken@ionic.io>
2022-12-08 09:25:47 -0600

    fix(aws): remove offline_access scope from AWS

--- src/services/oidc-authentication-service.ts
+++ src/services/oidc-authentication-service.ts
@@ -33,7 +33,7 @@ const baseAWSConfig = {
   authConfig: 'cognito' as const,
   clientID: '64p9c53l5thd5dikra675suvq9',
   discoveryUrl: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_YU8VQe29z/.well-known/openid-configuration',
-  scope: 'openid email profile offline_access',
+  scope: 'openid email profile',
   audience: '',
 };
 
